c808ab9387afc8cf2f553591dba64d21711d2ad4
Lekkioui <anassepaco@gmail.com>
2026-03-29 22:56:13 +0100

    Update setup_test_data.py

--- flights/management/commands/setup_test_data.py
+++ flights/management/commands/setup_test_data.py
@@ -1,25 +1,21 @@
-# ../airline-django/flights/management/commands/setup_test_data.py
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from flights.models import Airport, Flight, Passenger
 
 class Command(BaseCommand):
-    help = "Configure les données pour les tests automatisés"
-
     def handle(self, *args, **kwargs):
-        # Nettoyage
-        User.objects.all().delete()
+        # Suppression propre (Ordre important pour les clés étrangères)
+        Passenger.objects.all().delete()
+        Flight.objects.all().delete()
         Airport.objects.all().delete()
+        User.objects.filter(username='anasse').delete()
 
-        # Admin
+        # Création des données de base
         User.objects.create_superuser('anasse', 'anasse@gmail.com', 'anasse')
-
-        # Data
         jfk = Airport.objects.create(code='JFK', city='New York City')
         lhr = Airport.objects.create(code='LHR', city='London')
         f = Flight.objects.create(origin=jfk, destination=lhr, duration=415)
         
+        # On crée exactement 10 passagers pour tes tests de logique
         for i in range(10):
             Passenger.objects.create(first=f'Passenger{i}', last='Test')
-            
-        self.stdout.write(self.style.SUCCESS('Données de test initialisées avec succès'))
