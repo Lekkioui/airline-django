55326afd2e9e6e1fc3f834fac7b89ffa8d06809b
Lekkioui <anassepaco@gmail.com>
2026-04-03 18:25:14 +0100

    fix: redirect to flights after login, update url names

--- users/views.py
+++ users/views.py
@@ -1,31 +1,32 @@
-
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
 
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
-    return render(request , 'users/user.html')
+    return HttpResponseRedirect(reverse('index_flights'))
+
 
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request,username = username,password=password)
+        user = authenticate(request, username=username, password=password)
         if user:
-            login(request,user)
-            return HttpResponseRedirect(reverse("index"))
+            login(request, user)
+            return HttpResponseRedirect(reverse('index_flights'))
         else:
-            return render(request , 'users/login.html',{
-                "message":"Invalid credential"
+            return render(request, 'users/login.html', {
+                "message": "Invalid credentials"
             })
-    return render(request , 'users/login.html')
+    return render(request, 'users/login.html')
+
+
 def logout_view(request):
     logout(request)
-    return render(request , 'users/login.html' , {
-        "message":"Logged Out"
+    return render(request, 'users/login.html', {
+        "message": "Logged Out"
     })
